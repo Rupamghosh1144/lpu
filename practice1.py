80235dae73aba445481c8058c9aeb196a70e0e11
Rupamghosh1144 <115186452+Rupamghosh1144@users.noreply.github.com>
2022-10-14 17:35:18 +0530

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,30 @@
+#array location
+#x="This is LPU"
+#print(x[2:])     #2 number theke puro sob ta print hobe
+#print(x[:7])      #1st theke 6 number letter porjonto print hobe
+#print(x[0:7])     #0(1st) theke 6th letter porjonto print hobe
+#print(x[-4:-1])   #negetive arry
+
+
+#modify the index
+#x="This is Rupam"
+#print(x.upper())   #Etate sob lekha capital hoye Jabe
+#print(x.lower())    #etate sob lekha small hoye jabe
+
+#split the text
+#x="This is west Bengal"
+#splittedText=x.split()          #(splittedText use for to indicte each ward)
+#print(splittedText[0])
+#print(splittedText[1])
+#print(splittedText[3])
+
+#replace
+#x="This is Punkab"
+#print(x.replace("k","j"))
+
+x=input(float)
+y=100
+if(x<y)
+print True
+else
+print False
