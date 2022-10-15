dbf6b5f48941f9beab79d5c904e3779ed5e39345
Rupam Ghosh <115186452+Rupamghosh1144@users.noreply.github.com>
2022-10-15 22:34:38 +0530

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,51 @@
+#pyhton arthmetic oprtors
+#print(10+100) #addition
+#print(100-0) #substarction
+#print(100*10) #multiplication
+#print(100/10) #devide
+#print(100%10) #modulas
+#print(21//6) #floor
+
+
+#assign operator
+#x=30
+#x +=10
+#x -=10
+#x *=10
+#x /=10
+#x %=10
+#x //=10 
+
+#exponential operator
+#x = (3**3) 
+#print(x)
+#y = (100**4)
+#print(y)
+
+#comprison
+#print(3==4)
+#print(3>4)
+#print(45<55)
+#print(45<=55)
+
+#Logical operator
+#x=10
+#print(x<20 and x<30)
+#print(x<5 and x<3)
+#print(x<5 and x<11)
+
+#idnenty operator
+#a=[10,20]
+#b=[11,12]
+#c=a
+#d=b
+#print(c)
+
+a=[10,20]
+b=[11,12]
+c=a
+d=b
+a.remove(20)
+b.remove(11)
+print(c)
+print(d)
