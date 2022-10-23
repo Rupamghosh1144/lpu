e2edaa1b16bb134b3afc411362c4e8b30b4066ea
Rupam Ghosh <115186452+Rupamghosh1144@users.noreply.github.com>
2022-10-23 19:35:56 +0530

    Add files via upload

---
+++ 23oct.py
@@ -0,0 +1,70 @@
+#logical operator (or)
+#x=20
+#print(x<50 or x>50)
+#print(x<50  or x>10)
+#print(x<10 or x>40)
+
+#logical not oprator(not operator means true startement output is false and false satement output is true)
+#x=20
+#print(not(x<30 and x>10))  #one statemet is wrong output is also wrong
+#print(not(x>40 and x<10))  #not means true statement output is false and false statement output is true
+#print(not(x<10 and x>33))
+
+#Identify Operator
+#x=[10,20]
+#y=[10,20]
+#z=x
+#print(x is z)
+
+#REMOVE operator
+#x=[10,20]
+#y=[10,20]
+#z=x
+#x.remove(20)
+#y.remove(10)
+#print(x)
+#print(y)
+
+#is not operator   ##not operator symbol is("!=")
+#a=[10,20]
+#b=[10,20]
+#c=a
+#print(a is not c)
+#print(b is not c)
+#print(a != c)
+
+#Membership operator
+#a=[10,20,30,40]
+#print(20 in a)
+#print(10 not in a)
+#print(100 in a )
+#print(100 not in a)
+
+#Bitwise operartor
+#Example of OR operator  ##sign of or operator("|")
+#x=10
+#y=20
+#print(x|y)
+
+#Example of XOR operato    ##sign of XOR opetrator("^")
+#x=10
+#y=20
+#print(x^y)
+
+#Example of NOT operator   ##sign of NOT operator("~")  [A=-A(A+1)]
+#x=10
+#y=20
+#print(~y)
+#print(~x)
+
+#Example of left side operator  ##sign of Left side operator("<<")
+#x=17
+#print(x<<3)
+#print(x<<4)
+#print(x<<2)
+
+#Example of right Operator   ##sign of right side operator(">>")
+#x=17
+#print(x>>3)
+#print(x>>4)
+#print(x>>2)
