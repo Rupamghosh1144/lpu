1656a8512973f386e3afb1aa42bed7c33e98de5c
Rupam Ghosh <115186452+Rupamghosh1144@users.noreply.github.com>
2022-10-26 01:13:44 +0530

    Add files via upload

---
+++ 26oct.py
@@ -0,0 +1,38 @@
+#print("hello world")  
+
+
+#x="Hello"
+#y="World"
+#print(x)
+#print(y)
+#print(x+y)
+#print(x,y)
+
+
+x=10
+y="Hello India"
+z=x+15
+a="Hello Dubai"
+b="Hello Dubai"+y
+print(x+y)   #this is not possible bcoz [str+int=error]
+print(x,y)
+print(x**2)
+print(x/5)
+print(z)
+print(z/5)
+print("hello west bengal"+y)
+print("hello kolkata",y)
+print(25+z)
+print(y.replace('India','USA'))
+print(a)
+print(y,a)
+print(y,b)
+print("hello Japan",y,a)
+print(z**3)
+print(z*3)
+print(float(z*2))
+print(float(z/2))
+print(float(z//2))
+print(z%2)
+print(x|2)
+print("Good night lpu,This time is",e,"pm")
