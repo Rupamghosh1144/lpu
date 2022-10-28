63ad3e523de4d7529af5905cfac90648009fcc63
Rupam Ghosh <115186452+Rupamghosh1144@users.noreply.github.com>
2022-10-28 17:32:12 +0530

    Delete apna2.py

--- apna2.py
+++
@@ -1,9 +0,0 @@
-#if else statement
-age=int(input('enter your age: '))
-if age >=18:
-    print('You can Vote')
-elif age <= 18 and age >=13:
-    print('You age in teenagers')
-else:
-    print('You are not adult')
-    
