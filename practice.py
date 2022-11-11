3599eea44fe111fd3132e7c7dd69e98ccdce8e5c
Rupam Ghosh <115186452+Rupamghosh1144@users.noreply.github.com>
2022-11-11 19:31:56 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,120 @@
+#x="Rupam "
+#y="Ghosh"
+#print(x+y)
+
+#myfirstname="Rupam"
+#print(myfirstname)
+
+
+#x=y=z=100
+#print(x+y)
+#print(x,y,z)
+
+#x=19
+#def myfunc():
+#    print("My age is"+x)
+
+
+#x="Sultannagar"
+#print(x[3])
+#print(x[-3])
+
+
+#name="Sultannagar"
+#print(name.find(r))
+
+#x="sultannagar"
+#print(x.upper())
+#print(x.lower())
+
+
+#x="Supam"
+#print(x.replace("S","R"))
+
+
+#print(10>5)
+#print(10==10)
+
+#mylist=[10,20,30,40]
+#print(mylist)
+
+##x=10
+#y=17
+#print(x & y)
+#print(x|y)
+
+
+#x=int(input("Entrt the second value:"))
+#if(10>=x):
+#    print("True")
+#else:
+#    print("False")
+
+#x=200
+#y=100
+#if (x>y):
+#    pass
+#else:
+#    print("This is LPU")
+
+
+#for x in range(1,10,2):
+#    print(x)
+
+#def myFunction():
+#    print("Hello")
+#    print("Hi")
+#    print("bye")
+#print(1)
+#myFunction()
+
+#print("This is secomd time")
+#myFunction()
+
+
+#print("This is third time")
+#myFunction()
+
+
+
+#def myFunction():
+#    print("my name is"+"john")
+#myFunction()
+
+
+#def myApple(name):
+#    print("My favorite fruit is"+name)
+#myApple(" Banana")  
+
+
+my_list = [100,200,300,400,500,600,700,800,900]
+my_list[4] = 'FIVE HUNDRED'
+print (my_list)
+
+# CAHANGE A RANGE OF ITEM LIST
+my_list = [100,200,300,400,500,600,700,800,900]
+my_list[4:6] = ['FIVE HUNDRED','SIX HUNDRED']
+print (my_list)
+
+#insert value in string
+my_list= ['i','love','INDIA']
+my_list.insert(2, 'my')
+print (my_list)
+
+#insert value in string
+my_list= ['i','love','my','family']
+my_list.append('and')
+my_list.append('my')
+my_list.append('INDIA')
+print (my_list)
+
+#POP 
+my_list = ["Rupam","Sayan","Priyajit"]
+my_list.pop(2)
+my_list.pop(1)
+print (my_list)
+
+#Clear function
+my_list = ["Rupam","Sayan","Priyajit"]
+my_list.clear()
+print (my_list)
